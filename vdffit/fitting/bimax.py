7b172ebce8ac31b9f5fa8e61d6eb476070f9d2b8
David Stansby <dstansby@gmail.com>
2021-11-11 09:35:01 +0000

    Tidy up status codes

--- vdffit/fitting/bimax.py
+++ vdffit/fitting/bimax.py
@@ -11,6 +11,7 @@ class BiMaxFitter(FitterBase):
 
     def status_info(self):
         return {2: "Less than 12 points available for fit.",
+                3: "Velocity at peak VDF is non-finite."
                 4: "Fitted velocity is out of the VDF bounds."}
 
     @staticmethod
@@ -55,7 +56,7 @@ class BiMaxFitter(FitterBase):
 
         guesses = self.initial_guesses(vs, vdf)
         if np.any(np.isnan([guesses[1], guesses[2], guesses[3]])):
-            return 5, {}
+            return 3, {}
 
         # Do fitting
         fitout = opt.least_squares(resid, guesses,
@@ -64,14 +65,16 @@ class BiMaxFitter(FitterBase):
 
         fitparams = fitout.x
 
-        v = fitparams[1:4]
-        out_of_bounds = [(v[i] < np.min(vs[:, i]) or
-                          v[i] > np.max(vs[:, i]))
+        v_bulk = fitparams[1:4]
+        out_of_bounds = [(v_bulk[i] < np.min(vs[:, i]) or
+                          v_bulk[i] > np.max(vs[:, i]))
                          for i in range(3)]
         out_of_bounds = np.any(out_of_bounds)
         if out_of_bounds:
             return 4, {}
 
+        # Transform bulk velocity out of field aligned frame
+        fitparams[1:4] = np.einsum('ij,j->i', R.T, v_bulk)
         return 1, {k: v for k, v in zip(self.fit_param_names, fitparams)}
 
     def initial_guesses(self, vs, vdf):
@@ -86,4 +89,4 @@ class BiMaxFitter(FitterBase):
         peak_idx = np.nanargmax(vdf)
         A0 = vdf[peak_idx]
         v0 = vs[peak_idx, :]
-        return [A0, v0[0], v0[1],  v0[2], 40, 40]
+        return [A0, v0[0], v0[1], v0[2], 40, 40]
